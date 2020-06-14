2b8e542640b102d0b54e865cf90556fbb6ca58cc
AugustinJose1221 <augustinjose1221@gmail.com>
2020-06-14 12:37:00 +0530

    Added Difference/FilterDiff.py

---
+++ Difference/FilterDiff.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 12:03:58 2020
+
+@author: augustinjose
+"""
+
+
+import numpy as np
+
+def filterDiff(img1, img2):
+    img_height, img_width = img1.shape[:2]
+    channel = img1.shape[2]
+    diff = np.empty([img_height, img_width, channel])
+    pixel = 0
+    for i in range(img_height):
+        for j in range(img_width):
+            for k in range(channel):
+                pixel = img1[i][j][k] - img2[i][j][k]
+                if pixel < 0:
+                    pixel = 0
+                diff[i][j][k] = pixel
+                
+    return diff
